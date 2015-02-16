6e25df098fc850a422bb08fbd9c0f1bb74f95952
RicardoBorgesGO <ricardoborges.ufg@gmail.com>
2015-02-16 14:34:27 -0200

    Remoção das entidades do projeto, para separar as entidades do modelo de domínio.

--- src/main/java/com/upschool/rest/EventoRest.java
+++ src/main/java/com/upschool/rest/EventoRest.java
@@ -2,10 +2,13 @@ package com.upschool.rest;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -25,5 +28,13 @@ public class EventoRest {
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Evento> getEventos() {
 		return eventoService.listarTodosOsEventos();
+	}
+
+	@POST
+	@Path("/setEvento")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addEvento(final Evento evento) {
+
+		return null;
 	}
 }
